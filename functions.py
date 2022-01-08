1dcb44b62eddc8f9382b36ce78a931555a22d0c4
dnoitsis <36934201+dnoitsis@users.noreply.github.com>
2022-01-08 19:25:07 +0200

    functions file added22

--- functions.py
+++ functions.py
@@ -3,7 +3,7 @@ import pandas as pd
 import math
 
 
-# calculates the cost for laying the network.
+# calculates the cost for laying the network.aaa
 def calculate_laying_cost(links, links_location):
     total_cost = 0
     for link in links:
